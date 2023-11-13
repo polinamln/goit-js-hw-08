bcd2988d4f5e7e045a78de6d4d7b4c974134090a
polinamln <polina77979.la@gmail.com>
2023-11-13 16:00:03 +0100

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,20 +1,36 @@
 import { saveToLs, loadFromLs } from './helpers';
+import throttle from 'lodash.throttle';
 
 const refs = {
-   iframe: document.querySelector('#vimeo-player')
- 
-}
- const player = new Vimeo.Player(refs.iframe);
+  iframe: document.querySelector('#vimeo-player')
+};
+
+const player = new Vimeo.Player(refs.iframe);
 
 function getCurrentTime() {
-    return player.getCurrentTime();
+  return player.getCurrentTime();
 }
 
-const currentTime = getCurrentTime();
-saveToLs('videoplayer-current-time', currentTime);
+const saveCurrentTimeToLS = throttle(function () {
+  const currentTime = getCurrentTime();
+  saveToLs('videoplayer-current-time', currentTime);
+}, 1000); 
 
+function loadVideoFromLS() {
+  const storedTime = loadFromLs('videoplayer-current-time');
 
-player.on('timeupdate', function (data) {
+  if (storedTime !== null) {
+    player.setCurrentTime(parseFloat(storedTime));
+    player.play();
+  }
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+  player.on('loaded', function () {
+    loadVideoFromLS();
+  });
+
+  player.on('timeupdate', function () {
+    saveCurrentTimeToLS();
+  });
 });
-
