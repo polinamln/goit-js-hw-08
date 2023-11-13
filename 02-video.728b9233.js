d1bcbc3a10833dd3622a7ecf9fcff68319dec3ae
polinamln <137650718+polinamln@users.noreply.github.com>
2023-11-13 15:00:29 +0000

    Deploying to gh-pages from @ polinamln/goit-js-hw-08@bcd2988d4f5e7e045a78de6d4d7b4c974134090a 🚀

--- 02-video.728b9233.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={iframe:document.querySelector("#vimeo-player")},r=new Vimeo.Player(e.iframe);var t,i,a=r.getCurrentTime();t="videoplayer-current-time",i=a,localStorage.setItem(t,JSON.stringify(i)),r.on("timeupdate",(function(e){}))}();
-//# sourceMappingURL=02-video.728b9233.js.map
--- 02-video.b97e158a.js
+++
@@ -1,2 +0,0 @@
-const e={iframe:document.querySelector("#vimeo-player")},t=new Vimeo.Player(e.iframe);const r=t.getCurrentTime();var i,o;i="videoplayer-current-time",o=r,localStorage.setItem(i,JSON.stringify(o)),t.on("timeupdate",(function(e){}));
-//# sourceMappingURL=02-video.b97e158a.js.map
