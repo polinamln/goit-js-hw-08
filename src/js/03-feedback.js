632345cc10c0a9331bb0a9b79cca73d1706c4566
polinamln <polina77979.la@gmail.com>
2023-11-14 16:37:34 +0100

    a

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,9 +11,10 @@ refs.form.addEventListener('submit', formSubmit);
 refs.form.addEventListener('input', throttle(formInput, 500));
 
 function formInput(event) {
+    const key = event.target.name;
     const value = event.target.value;
 
-    saveToLs("feedback-form-state", value);
+    saveToLs(key, value);
 };
 
 function formSubmit(event) {
@@ -21,6 +22,10 @@ function formSubmit(event) {
 
     const email = refs.form.elements.email.value;
     const message = refs.form.elements.message.value;
+
+    if (!email || !message) {
+        alert('Заповніть форму.')
+    }
 
     const obj = {
         email,
@@ -30,12 +35,13 @@ function formSubmit(event) {
     console.log(obj);
     
     event.target.reset();
-    localStorage.removeItem('feedback-form-state');
+    localStorage.removeItem('email');
+    localStorage.removeItem('message');
 };
 
 function onLoad() {
-    const email = loadFromLs('feedback-form-state');
-    const message = loadFromLs('feedback-form-state');
+    const email = loadFromLs('email');
+    const message = loadFromLs('message');
 
     refs.form.elements.email.value = email;
     refs.form.elements.message.value = message;
